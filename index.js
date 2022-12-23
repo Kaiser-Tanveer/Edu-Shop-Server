92710d3ae76ceabd898f082c8e4d0598090858d2
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2022-12-24 00:44:20 +0600

    finding pen category from database

--- index.js
+++ index.js
@@ -1,31 +1,80 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express());
+// app.use(cors());
+const corsConfig = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.use(express.json({ limit: "30mb" }));
+// app.use(express.json());
 
 // MongoDB Connection 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 const run = () => {
     try {
+        // Collections 
+        const categoriesCollection = client.db("EduShop").collection("categories");
+        const productsCollection = client.db("EduShop").collection("products");
+
+
+        // getting categories 
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        })
+
+
+        // getting products 
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const products = await productsCollection.find(query).toArray();
+            res.send(products);
+        })
+
+        // getting specific products by category
+        app.get('/products/:name', async (req, res) => {
+            const name = req.params.name;
+            const filter = { category_name: name };
+            const result = await productsCollection.find(filter).toArray();
+            res.send(result);
+        })
+
+        // getting specific products by category
+        app.get('/homeProduct', async (req, res) => {
+            const query = { category_name: 'pens' }
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        // getting specific products 
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: ObjectId(id) };
+            const result = await productsCollection.findOne(filter);
+            res.send(result);
+        })
 
     }
     finally {
 
     };
 };
-run().catch(err => console.log(err));
+run()
 
 app.get('/', (req, res) => {
     res.send('Edu-Shop is Running..')
